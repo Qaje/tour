a0f0439b7c4e97b1b7b5c940a17f05ad410b9c62
qaje <javierpal3232@gmail.com>
2018-05-05 16:28:49 -0400

    mutiple AUTH

--- routes/web.php
+++ routes/web.php
@@ -25,4 +25,11 @@ Route::get('/users/logout','Auth\LoginController@userLogout')->name('user.logout
 		Route::post('/login', 'Auth\AdminLoginController@login')->name('admin.login.submit');
 		Route::get('/', 'AdminController@index')->name('admin.dashboard');
 		Route::get('/logout','Auth\AdminLoginController@logout')->name('admin.logout');
+
+		//Passsword reset routes
+		Route::post('password/email','Auth\AdminForgotPasswordController@sendResetLinkEmail')->name('admin.password.email');
+		Route::get('password/reset','Auth\AdminForgotPasswordController@showLinkRequestForm')->name('admin.password.request');
+		Route::post('password/reset','Auth\AdminResetPasswordController@reset');
+		Route::get('password/reset/{token}','Auth\AdminResetPasswordController@showResetForm')->name('admin.password.reset');
+
 	});
